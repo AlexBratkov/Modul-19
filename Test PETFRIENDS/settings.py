ca2666d667a6a093dfbe931b190ca6fec564b867
AlexBratkov <94742437+AlexBratkov@users.noreply.github.com>
2022-02-27 00:49:54 +1000

    Add files via upload

---
+++ Test PETFRIENDS/settings.py
@@ -0,0 +1,12 @@
+valid_email = "sanches1305@inbox.ru"
+valid_password = "1305"
+
+invalid_email = "13rus.com"
+invalid_password = "12345"
+
+invalid_key = {
+  "key": ""
+}
+
+invalid_pet_id = '*'
+empty_pet_id = ''
